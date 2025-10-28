b6f951832304696dc466b47200a339aaa8b75afe
julito <julito@local>
2025-10-28 05:45:29 -0300

    feat: add TidepoolApi submodule

---
+++ src/app/appointments/appointments-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppointmentsPage } from './appointments.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppointmentsPage
+  },
+  {
+    path: 'appointment-detail',
+    loadChildren: () => import('./appointment-detail/appointment-detail.module').then( m => m.AppointmentDetailPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AppointmentsPageRoutingModule {}
